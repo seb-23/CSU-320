b748e9e31a5cea19b1422cd03a02f7445c72db8e
Sebastian Infantes <70537133+seb-23@users.noreply.github.com>
2021-10-29 17:14:38 -0600

    Create randSM.py

---
+++ HW1/randSM.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def randomSM (n, file_name):
+	"""    The function will generate n men and n women with names 1 to n. 
+	It assignes a random preference list to each of the men;
+	of all the men in order and stores them in the file with the
+	given filename in the format described in comments to read_from_file().
+	"""
+	
+	 Men_pref_list = []
+	 
